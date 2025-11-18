3c387c0f77089e13aff31475d6a8cb5904bd65f3
Shahroz Javed <shahrozmaher198r@gmail.com>
2025-11-18 16:54:45 +0500

    pulse, telescope and horizon setup

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
+    App\Providers\TenancyServiceProvider::class,
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Models\Tenant;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        // your actual routes
+        Route::get('/', function () {
+            // dd(request()->getHost());
+            // $t = Tenant::create();
+            // $t->domains()->create(['domain' => 'tenant112.localhost']);
+            // return $t;
+            return view('welcome');
+
+        });
+    });
+}
+
+// require __DIR__ . '/tenant.php';
